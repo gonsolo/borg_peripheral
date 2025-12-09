b0a014beef7097f9189ec2a0eff700793a42265a
Gon Solo <gonsolo@gmail.com>
2025-12-09 16:55:15 +0100

    Test simple integer addition.
    Gemini was used to get a basic understanding of the role of SPI in this
    context and to generate a basic test.
    https://gemini.google.com/app/665f6cf3e1cbeb51

--- borg/src/Borg.scala
+++ borg/src/Borg.scala
@@ -7,81 +7,51 @@ import chisel3._
 import chisel3.util.{MuxCase, Cat}
 import hardfloat._
 
+import chisel3._
+import chisel3.util._
+
 class Borg extends Module {
-  val io = IO(new Bundle {
-    val ui_in = Input(UInt(8.W))
-    val uo_out = Output(UInt(8.W))
-    val address = Input(UInt(6.W))
-    val data_in = Input(UInt(32.W))
-    val data_write_n = Input(UInt(2.W))
-    val data_read_n = Input(UInt(2.W))
-    val data_out = Output(UInt(32.W))
-    val data_ready = Output(Bool())
-    val user_interrupt = Output(Bool())
-  })
+    val io = IO(new Bundle {
+        val ui_in = Input(UInt(8.W))
+        val uo_out = Output(UInt(8.W))
+        val address = Input(UInt(6.W))
+        val data_in = Input(UInt(32.W))
+        val data_write_n = Input(UInt(2.W))
+        val data_read_n = Input(UInt(2.W))
+        val data_out = Output(UInt(32.W))
+        val data_ready = Output(Bool())
+        val user_interrupt = Output(Bool())
+    })
 
-  io.data_out := io.data_in
+    val operand_A = RegInit(0.U(32.W))
+    val operand_B = RegInit(0.U(32.W))
 
-  io.uo_out := DontCare
-  io.data_ready := DontCare
-  io.user_interrupt := DontCare
+    val ADDR_A = 0.U(6.W)
+    val ADDR_B = 4.U(6.W)
+    val ADDR_RESULT = 8.U(6.W)
 
-  //val example_data = RegInit(0.U(32.W))
+    val is_write = io.data_write_n === "b10".U
 
-  //val next_example_data = MuxCase(example_data, Seq(
-  //  // 32-bit write, highest priority
-  //  (io.address === "h0".U && io.data_write_n === "b10".U) -> io.data_in,
-  //  // 16-bit write
-  //  (io.address === "h0".U && io.data_write_n === "b01".U) -> Cat(example_data(31, 16), io.data_in(15, 0)),
-  //  // 8-bit write
-  //  (io.address === "h0".U && io.data_write_n === "b00".U) -> Cat(example_data(31, 8), io.data_in(7, 0))
-  //))
+    when (is_write) {
+        when (io.address === ADDR_A) {
+            operand_A := io.data_in
+        }
+        .elsewhen (io.address === ADDR_B) {
+            operand_B := io.data_in
+        }
+    }
 
-  //// Assign the next state to the register on the clock edge.
-  //example_data := next_example_data
+    val sum = operand_A + operand_B
 
-  //io.uo_out := example_data(7, 0) + io.ui_in
+    io.data_out := MuxLookup(io.address, 0.U) (Seq(
+      ADDR_A      -> operand_A,
+      ADDR_B      -> operand_B,
+      ADDR_RESULT -> sum
+    ))
 
-  //io.data_ready := true.B
+    io.data_ready := true.B
 
-  //val example_interrupt = RegInit(false.B)
-  //val last_ui_in_6 = RegInit(false.B)
-
-  //when(io.ui_in(6) && !last_ui_in_6) {
-  //  example_interrupt := true.B
-  //}.elsewhen(io.address === "h8".U && io.data_write_n =/= "b11".U && io.data_in(0) === 1.U) {
-  //  example_interrupt := false.B
-  //}
-
-  //last_ui_in_6 := io.ui_in(6)
-
-  //io.user_interrupt := example_interrupt
-
-  //// Adder
-  //val add_a = RegInit(0.U((Globals.expWidth + Globals.sigWidth).W))
-  //val add_b = RegInit(0.U((Globals.expWidth + Globals.sigWidth).W))
-  //add_a := example_data(31, 16)
-  //add_b := example_data(15, 0)
-  //val roundingMode = RegInit(consts.round_near_even)
-  //val detectTininess = RegInit(false.B)
-
-  //val addRecFN = Module(new AddRecFN(Globals.expWidth, Globals.sigWidth))
-  //addRecFN.io.subOp := false.B
-  //addRecFN.io.a := recFNFromFN(Globals.expWidth, Globals.sigWidth, add_a)
-  //addRecFN.io.b := recFNFromFN(Globals.expWidth, Globals.sigWidth, add_b)
-  //addRecFN.io.roundingMode   := roundingMode
-  //addRecFN.io.detectTininess := detectTininess
-
-  //val add_result = RegInit(0.U((Globals.expWidth + Globals.sigWidth).W))
-  //add_result := addRecFN.io.out
-
-  //// Address 0 reads the example data register.
-  //// Address 4 reads ui_in.
-  //// All other addresses read 0.
-  //io.data_out := MuxCase(0.U, Seq(
-  //  (io.address === "h0".U) -> example_data,
-  //  (io.address === "h4".U) -> io.ui_in.pad(32),
-  //  (io.address === "h8".U) -> Cat(add_result, 0.U(16.W))
-  //))
+    io.uo_out := DontCare
+    io.user_interrupt := false.B
 }
 
--- borg/test/src/BorgTests.scala
+++ borg/test/src/BorgTests.scala
@@ -4,62 +4,46 @@ import chisel3._
 import chisel3.simulator.EphemeralSimulator._
 import utest._
 
+import utest._
+import chisel3._
+import chisel3.experimental.BundleLiterals._
+
 object BorgTests extends TestSuite {
-  val tests = Tests {
-    utest.test("borg_float_add") {
-      simulate(new Borg) { borg =>
+    val tests = Tests {
+        utest.test("borg_integer_add") {
+            simulate(new Borg) { borg =>
+                borg.io.data_write_n.poke("b11".U)
+                borg.io.data_read_n.poke("b11".U)
+                borg.io.address.poke(0.U)
+                borg.clock.step(1)
 
-        val a = 5.0f
-        val a_int = java.lang.Float.floatToIntBits(a)
-        borg.io.data_in.poke(a_int.U)
-        //borg.clock.step(1)
-        val b_big_int = borg.io.data_out.peek().litValue
-        val b = java.lang.Float.intBitsToFloat(b_big_int.toInt)
-        println(s"The value of b is: $b")
-        utest.assert(b == a)
+                val A = 123.U
+                borg.io.address.poke(0.U)
+                borg.io.data_in.poke(A)
+                borg.io.data_write_n.poke("b10".U)
+                borg.clock.step(1)
 
-        //val input_a_float = 10.5f
-        //val input_b_float = 5.25f
-        //val expected_result_float = input_a_float + input_b_float
+                val B = 456.U
+                borg.io.address.poke(4.U)
+                borg.io.data_in.poke(B)
+                borg.io.data_write_n.poke("b10".U)
+                borg.clock.step(1)
 
-        //val input_a_bits = java.lang.Float.floatToIntBits(input_a_float)
-        //val input_b_bits = java.lang.Float.floatToIntBits(input_b_float)
-        //val expected_bits = java.lang.Float.floatToIntBits(expected_result_float)
+                borg.io.data_write_n.poke("b11".U)
 
-        //borg.io.address.poke("h0".U)
-        //borg.io.data_in.poke(input_a_bits.U)
-        //borg.io.data_write_n.poke("b10".U)
-        //borg.clock.step(1)
+                val expected_sum = 123 + 456
 
-        //val val_a = 5.0f
-        //val val_b = 2.0f
-        //val expected = val_a + val_b
+                borg.io.address.poke(8.U)
+                borg.clock.step(1)
 
-        //val input_a_16bit_rep = java.lang.Float.floatToIntBits(val_a)
-        //val input_b_16bit_rep = java.lang.Float.floatToIntBits(val_b)
-        //val combined_input = (input_a_16bit_rep << 16) | (input_b_16bit_rep & 0xFFFF)
-        //
-        //borg.io.address.poke("h0".U)
-        //borg.io.data_in.poke(combined_input.U)
-        //borg.io.data_write_n.poke("b10".U)
+                val actual_sum = borg.io.data_out.peek().litValue.toInt
 
-        //borg.io.data_read_n.poke("b11".U)
-        //borg.clock.step(1)
-        //borg.clock.step(100) 
-        //borg.io.address.poke("h8".U)
-        //borg.io.data_write_n.poke("b11".U)
-        //borg.io.data_read_n.poke("b01".U)
+                println(s"Operand A: 123, Operand B: 456")
+                println(s"Expected Sum: $expected_sum, Actual Sum: $actual_sum")
 
-        //val actual_output_bits_32bit_container = borg.io.data_out.peek().litValue
-        //val actual_result_bits = (actual_output_bits_32bit_container >> 16).toInt
-        //val actual_result_float = java.lang.Float.intBitsToFloat(actual_result_bits)
-
-        //val epsilon = 0.0001f
-        ////utest.assert(Math.abs(actual_result_float - expected) < epsilon)
-        //// Wrong for now
-        //utest.assert(actual_result_float == 0.0)
-      }
+                utest.assert(actual_sum == expected_sum)
+            }
+        }
     }
-  }
 }
 
