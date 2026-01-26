34382e31ffc35e452f5d5bb59e4494965c5ae051
Gon Solo <gonsolo@gmail.com>
2026-01-26 11:50:10 +0100

    Use register file.

--- borg/src/Borg.scala
+++ borg/src/Borg.scala
@@ -12,37 +12,39 @@ class Borg extends Module {
         val data_out     = Output(UInt(32.W))
         val data_ready   = Output(Bool())
         // Unused TinyQV signals
-        val ui_in        = Input(UInt(8.W))
-        val uo_out       = Output(UInt(8.W))
+        val ui_in          = Input(UInt(8.W))
+        val uo_out         = Output(UInt(8.W))
         val user_interrupt = Output(Bool())
     })
 
-    val operand_A_bits = RegInit(0.U(32.W))
-    val operand_B_bits = RegInit(0.U(32.W))
+    val rf = RegInit(VecInit(Seq.fill(3)(0.U(32.W))))
 
     val ADDR_A   = 0.U(6.W)
     val ADDR_B   = 4.U(6.W)
     val ADDR_ADD = 8.U(6.W)
     val ADDR_MUL = 12.U(6.W)
+    val ADDR_C   = 16.U(6.W)
 
     val is_writing = io.data_write_n === "b10".U
     when(is_writing) {
         when(io.address === ADDR_A) {
-            operand_A_bits := io.data_in
+            rf(0) := io.data_in
         } .elsewhen(io.address === ADDR_B) {
-            operand_B_bits := io.data_in
+            rf(1) := io.data_in
+        } .elsewhen(io.address === ADDR_C) {
+            rf(2) := io.data_in
         }
     }
 
-    val recA = recFNFromFN(8, 24, operand_A_bits)
-    val recB = recFNFromFN(8, 24, operand_B_bits)
+    val recA = recFNFromFN(8, 24, rf(0))
+    val recB = recFNFromFN(8, 24, rf(1))
 
     val f_add = Module(new AddRecFN(8, 24))
-    f_add.io.subOp := false.B                          // False = Add
+    f_add.io.subOp := false.B
     f_add.io.a := recA
     f_add.io.b := recB
-    f_add.io.roundingMode := 0.U                       // round_near_even
-    f_add.io.detectTininess := 1.U                     // tininess_afterRounding
+    f_add.io.roundingMode := 0.U
+    f_add.io.detectTininess := 1.U
 
     val f_mul = Module(new MulRecFN(8, 24))
     f_mul.io.a := recA
@@ -54,10 +56,11 @@ class Borg extends Module {
     val mul_result = RegNext(fNFromRecFN(8, 24, f_mul.io.out))
 
     io.data_out := MuxLookup(io.address, 0.U)(Seq(
-        ADDR_A      -> operand_A_bits,
-        ADDR_B      -> operand_B_bits,
-        ADDR_ADD    -> add_result,
-        ADDR_MUL    -> mul_result
+        ADDR_A   -> rf(0),
+        ADDR_B   -> rf(1),
+        ADDR_C   -> rf(2),
+        ADDR_ADD -> add_result,
+        ADDR_MUL -> mul_result
     ))
 
     io.data_ready := true.B
